7ae4a5180a9a3b3cf67b3c412d6a1932827a7093
osni junior <osniandrade@gmail.com>
2021-09-14 00:18:26 +0000

    working on ft_chunklist

--- flow1.c
+++ flow1.c
@@ -6,7 +6,7 @@
 /*   By: ocarlos- <ocarlos-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 09:13:38 by ocarlos-          #+#    #+#             */
-/*   Updated: 2021/09/09 23:38:28 by ocarlos-         ###   ########.fr       */
+/*   Updated: 2021/09/14 00:18:26 by ocarlos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,39 +56,10 @@ void	ft_stack_big(t_stacks *data)
 // stack size > 5
 void	ft_stack_huge(t_stacks *data)
 {
-	int		c_list;
-	int		c_a;
-	int		c_b;
-	
-	c_list = 0;
 	if (ft_singlemove1(data))
 		return ;
 	if (ft_singlemove2(data))
 		return ;
-	while (!ft_ready(data))
-	{
-		c_a = 0;
-		c_b = data->a_heigth;
-		while (SA[c_a] != data->list[c_list] && SA[c_a] != data->list[c_list + 1])
-			c_a++;
-		while (SB[c_b] != data->list[data->a_heigth - c_list] &&
-				SB[c_b] != data->list[data->a_heigth - c_list - 1])
-			c_b--;
-		c_b = data->a_heigth - c_b;
-		if (c_a >= c_b)
-			while (c_a-- != 0)
-				ft_ra(data, TRUE);
-		else
-			while (c_b-- != 0)
-				ft_rra(data, TRUE);
-		if (data->a_heigth > 1)
-			ft_pb(data, TRUE);
-		if (data->b_heigth > 1 && SB[0] > SB[1])
-			ft_sb(data, TRUE);
-		c_list++;
-		if (data->a_heigth == 1)
-			while (data->b_heigth != 0)
-				ft_pa(data, TRUE);
-	}
+	
 	
 }
